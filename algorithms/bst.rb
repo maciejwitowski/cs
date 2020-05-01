17a8cee5312237718a397f14d55a4b8139061781
Maciej Witowski <witowski.maciej@gmail.com>
2020-05-01 21:46:55 +0200

    Count words

--- algorithms/bst.rb
+++ algorithms/bst.rb
@@ -52,16 +52,24 @@ class Bst
   end
 end
 
-Movie = Struct.new(:name, :rating)
+# Example: count words in text
+text = 'In computer science, binary search trees (BST), sometimes called ordered or sorted binary trees, are a particular type of container: a data structure that stores "items" (such as numbers, names etc.) in memory. They allow fast lookup, addition and removal of items, and can be used to implement either dynamic sets of items, or lookup tables that allow finding an item by its key (e.g., finding the phone number of a person by name).
+Binary search trees keep their keys in sorted order, so that lookup and other operations can use the principle of binary search: when looking for a key in a tree (or a place to insert a new key), they traverse the tree from root to leaf, making comparisons to keys stored in the nodes of the tree and deciding, on the basis of the comparison, to continue searching in the left or right subtrees. On average, this means that each comparison allows the operations to skip about half of the tree, so that each lookup, insertion or deletion takes time proportional to the logarithm of the number of items stored in the tree. This is much better than the linear time required to find items by key in an (unsorted) array, but slower than the corresponding operations on hash tables.
+Several variants of the binary search tree have been studied in computer science; this article deals primarily with the basic type, making references to more advanced types when appropriate.'
 
-movies = [
-  Movie.new("Avatar", 9),
-  Movie.new("Braveheart", 8),
-  Movie.new("Zorro", 3),
-  Movie.new("Bridget", 1),
-]
+words = text
+  .gsub!(/[^0-9A-Za-z ]/, '')
+  .split(' ')
+  .map(&:downcase)
 
 tree = Bst.new
-movies.each { |m| tree.put(m.name, m) }
 
-puts tree.get("Avatar")
+words.each do |w|
+  current = tree.get(w) || 0
+  tree.put(w, current + 1)
+end
+
+puts words
+  .map { |w| [w, tree.get(w)] }
+  .sort_by { |i| -i[1] }
+  .to_h
